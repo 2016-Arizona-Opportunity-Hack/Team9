609fe966d87e6074f2039b49903f2261b47321ee
Marshal Currier <marshalcurrier@gmail.com>
2016-10-01 19:25:31 -0700

    starting to break out headers and preparing to import data from Database

---
+++ index.php
@@ -0,0 +1,10 @@
+	<!-- Fixed navbar -->
+	<?php require_once('./functions/header.php'); ?>
+
+	<!-- Main jumbotron for a primary marketing message or call to action -->
+	<?php require_once('./functions/city.php');?>
+
+	<!-- Footer-->
+	<?php require_once('./functions/footer.php');?>
+
+	 <!-- /container -->
